400a7d53379f0e82d086f4e97bee0d84e50c8a62
alekssanya <777aleks_sanya777@bk.ru>
2024-01-04 22:59:05 +0600

    FINAL

--- npmInstall.js
+++ npmInstall.js
@@ -1,17 +1,33 @@
 const { exec } = require('node:child_process')
+const readline = require('readline');
 
-startMicroservice("m1")
-startMicroservice("m2")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+const myURLRabbitMQ = "amqps://eygbcnfp:fmSKEbpqkyfmi02ajeQvMMfhqPCLio35@kangaroo.rmq.cloudamqp.com/eygbcnfp"
+const portM1 = 8001
+const portM2 = 8002
+let firstProccessIsEnd = false
+let userURLRabbitMQ
+
+rl.question("Введите URL RabbitMQ или нажмите Enter: ", function (rabbitURL) {
+    let userURLRabbitMQ = rabbitURL
+    startMicroservice("m1")
+    startMicroservice("m2")
+    rl.close
+})
 
 function startMicroservice(serviceName) {
     exec(
-        `start cmd /k "cd ${serviceName}&&npm i"`,
+        `start cmd /k "cd ${serviceName}&&npm i&&echo Port=${serviceName == 'm1' ? portM1 : portM2} > .env&&echo RABBIT_URL=${userURLRabbitMQ || myURLRabbitMQ} >> .env&&exit"`,
         (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`)
-                return
-            }
+            exit()
         }
     )
+
 }
-exec('npm i')
+
+function exit() {
+    firstProccessIsEnd ? process.exit(0) : firstProccessIsEnd = true
+}
